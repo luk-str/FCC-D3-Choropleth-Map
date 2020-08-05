bb2ff458bb7c5b7d8abd4bd70e4364f4335f6f1b
luk-str <lstrociak@gmail.com>
2020-08-05 21:01:46 +0200

    Add Tooltips + Legend

--- main.js
+++ main.js
@@ -50,7 +50,6 @@ fetch(
 
         // Add Counties to Map
         const counties = map
-          .append("g")
           .selectAll("path")
           .data(countiesData)
           .enter()
@@ -62,7 +61,7 @@ fetch(
           .attr("stroke", "#fff")
           .attr("stroke-width", 0.3)
           .attr("fill", (d) => {
-            return `rgba(0, 100, 255, ${0 + (findEduPercentage(d) * 0.01)})`;
+            return `rgba(0, 100, 255, ${0 + findEduPercentage(d) * 0.012})`;
           });
 
         // Add States Outlines to Map
@@ -74,7 +73,73 @@ fetch(
           .append("path")
           .attr("d", d3.geoPath())
           .attr("fill", "none")
-          .attr("stroke", "#000")
-          .attr("stroke-width", 0.3);
+          .attr("stroke", "#fff")
+          .attr("stroke-width", 1);
+
+        // Define Tooltips
+        const tooltip = d3
+          .select(".map")
+          .append("g")
+          .attr("class", "tooltip")
+          .attr("id", "tooltip")
+          .style("opacity", 0);
+
+        // Add Tooltips on Mouseover
+        counties
+          .on("mouseover", function (d) {
+            const eduData = findEduData(d);
+            tooltip
+              .style("opacity", 1)
+              .attr("data-education", (i) =>
+                findEduPercentage(d)
+              ).html(`${eduData.area_name}, ${eduData.state}
+              <br>
+              ${eduData.bachelorsOrHigher}%`);
+          })
+          .on("mousemove", function (d) {
+            tooltip
+              .style("left", +d3.mouse(this)[0] + "px")
+              .style("top", +d3.mouse(this)[1] + 60 + "px");
+          })
+          .on("mouseout", (d) => {
+            tooltip.style("opacity", 0);
+          });
+
+        // Add Legend
+        const legend = map
+          .append("g")
+          .attr("class", "legend")
+          .attr("id", "legend")
+          .attr("transform", `translate(550, 0)`);
+
+        // Define Legend Colors
+        const legendColors = [];
+        for (let i = 0; i < 1; i += 0.05) {
+          legendColors.push(`rgba(0, 100, 255, ${i})`);
+        }
+
+        const legendWidth = 300;
+
+        // Define Legend Scale
+        const legendScale = d3
+          .scaleLinear()
+          .domain([0, 100])
+          .range([0, legendWidth]);
+
+        // Add Legend Bars
+        legend
+          .selectAll("rect")
+          .data(legendColors)
+          .enter()
+          .append("rect")
+          .attr("width", legendWidth / legendColors.length)
+          .attr("height", 30)
+          .attr("x", (d, i) => (i * legendWidth) / legendColors.length)
+          .attr("fill", (d) => d);
+
+        // Add Legend Scale
+        legend
+          .append("g")
+          .call(d3.axisTop(legendScale).tickFormat((d) => d + "%"));
       });
   });
